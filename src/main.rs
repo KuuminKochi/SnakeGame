8e01cccb7bbdbd6f81df21f96409100b882edcf6
KuuminKochi <yesimkurisu@gmail.com>
2022-06-26 00:28:56 +0800

    Removed everything, added Rect and Sprite Movement

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
 use bevy::prelude::*;
 
-pub struct HelloPlugin;
-struct GreetTimer(Timer);
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App){
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
-
-#[derive(Component)]
-struct Person;
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Tombstone".to_string()));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-
-    if timer.0.tick (time.delta()).just_finished() {
-        for name in query.iter(){
-            println!("Hello {}!", name.0);
-        }
-    }
-
-}
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
+        .add_system(sprite_movement)
         .run();
+}
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.25),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("player.png"),
+        transform: Transform::from_xyz(100., 0., 0.),
+        ..default()
+    })
+    .insert(Direction::Up);
+}
+
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut player, mut transform) in sprite_position.iter_mut() {
+        match *player {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
+
+        if transform.translation.y > 200. {
+            *player = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *player = Direction::Up;
+        }
+
+    }
 }
