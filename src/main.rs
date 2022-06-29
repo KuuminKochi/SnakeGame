62e3ce05113f69d0dd14783fc4dd6a7c5f6d2991
KuuminKochi <yesimkurisu@gmail.com>
2022-06-30 00:11:19 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 use bevy::prelude::*;
 
-mod setup;
-pub use crate::setup::{setup, sprite_movement};
-
-
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(sprite_movement)
-        .run();
+    App::new().run();
 }
