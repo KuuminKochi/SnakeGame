d6fef216e4da8636bc030738dac4f92d62aa4b97
KuuminKochi <yesimkurisu@gmail.com>
2022-07-08 22:41:18 +0800

    added snake movement

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::new()
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
+        .add_system(snake_movement)
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -31,4 +32,24 @@ fn spawn_snake(mut commands: Commands) {
             ..default()
         })
         .insert(SnakeHead);
+}
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down){
+            transform.translation.y -= 2.;
+        }
+    }
 }
